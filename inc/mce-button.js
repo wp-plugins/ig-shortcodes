325b958f1a9b51ab7393b23f79f2709cc16520a9
iografica <iografica@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-20 08:31:57 +0000

    core file
    git-svn-id: https://plugins.svn.wordpress.org/ig-shortcodes/trunk@1010447 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/mce-button.js
@@ -0,0 +1,88 @@
+(function() {
+	tinymce.PluginManager.add('ig_mce_button', function( editor, url ) {
+		editor.addButton( 'ig_mce_button', {
+			text: 'IG Shortcodes',
+			icon: false,
+			type: 'menubutton',
+			menu: [
+				// BUTTONS
+				{
+					text: 'Button',
+					onclick: function() {
+					editor.insertContent('[ig_button size="medium" color="gray" link="http://themes.iografica.it" target="_blank"]more info[/ig_button]');						
+					},	
+				},
+				// COLUMNS
+                {
+                    text: 'Columns',
+                    menu: [
+                    {
+                        text: '1/2-1/2',
+                        onclick: function() {
+					    editor.insertContent('[ig_columns]\
+                                              [ig_column position="half first"]Content[/ig_column]\
+                                              [ig_column position="half last"]Content[/ig_column]\
+                                              [/ig_columns]');						
+					},
+                    },
+                    {
+                        text: '1/3-1/3-1/3',
+                        onclick: function() {
+                        editor.insertContent('[ig_columns]\
+                                              [ig_column position="one-third first"]Content[/ig_column]\
+                                              [ig_column position="one-third"]Content[/ig_column]\
+                                              [ig_column position="one-third last"]Content[/ig_column]\
+                                              [/ig_columns]');						
+					},
+                    },
+                    {
+                        text: '1/4-1/4-1/4-1/4',
+                        onclick: function() {
+                        editor.insertContent('[ig_columns]\
+                                             [ig_column position="one-fourth first"]Content[/ig_column]\
+                                             [ig_column position="one-fourth"]Content[/ig_column]\
+                                             [ig_column position="one-fourth"]Content[/ig_column]\
+                                             [ig_column position="one-fourth last"]Content[/ig_column]\
+                                             [/ig_columns]');						
+					},
+                    }
+                           ]
+                },
+				// CLLEARFIX
+				{
+					text: 'Clearfix',
+					onclick: function() {
+					editor.insertContent('[ig_clearfix]');						
+					},	
+				},
+				// NOTICEBOX
+				{
+					text: 'Notice Box',
+					onclick: function() {
+					editor.insertContent('[ig_notice color="gray"]Content[/ig_notice]');						
+					},		
+				},
+				// TAB
+				{
+					text: 'Tabs',
+					onclick: function() {
+					editor.insertContent('[ig_tabs][ig_tab label="Title 1"]Content 1[/ig_tab][ig_tab label="Title 2"]Content 2[/ig_tab][/ig_tabs]');						
+					},	
+				},
+				// TOGGLE
+				{
+					text: 'Toggle',
+					onclick: function() {
+					editor.insertContent('[ig_toggle heading="Title" onload="closed"]Content[/ig_toggle]');						
+					},	
+				},
+                {
+                    text: '...more shortcodes',
+                    onclick: function() {
+                    window.open('http://www.yourdomain.com','_blank');
+                    },
+                },	
+			]
+		});
+	});
+})();
---
+++ inc/shortcodes.js
@@ -0,0 +1,47 @@
+jQuery(function() {
+	
+	/* Toggle */
+	jQuery('.toggle-active').each(function() {
+		jQuery(this).find('.toggle-content').show();
+	});
+	jQuery('.ig-shortcode-toggle .toggle-heading').click(function() {
+		var toggle = jQuery(this).parent('.ig-shortcode-toggle');
+		if(jQuery(this).parent('.ig-shortcode-toggle').parent('div').hasClass('ig-shortcode-toggle')) {
+			toggle.parent('div').find('.ig-shortcode-toggle').find('.toggle-content:visible').slideUp();
+			toggle.parent('div').find('.toggle-active').removeClass('toggle-active');
+			toggle.toggleClass('toggle-active');
+			toggle.find('.toggle-content').slideToggle(400);
+		} else {
+			toggle.toggleClass('toggle-active');
+			toggle.find('.toggle-content').slideToggle(400);
+		}
+	});
+	
+	
+	/* Tabs */
+	jQuery('.ig-shortcode-tabs').each(function() {
+		
+		jQuery(this).prepend('<div class="ig-shortcode-tab-buttons"></div>');
+		jQuery(this).find('.ig-shortcode-tabpane').each(function() {
+			
+			jQuery(this).parent('.ig-shortcode-tabs').find('.ig-shortcode-tab-buttons').append('<a href="#">'+jQuery(this).find('.ig-shortcode-tab-label').text()+'</a>');
+			jQuery(this).find('.ig-shortcode-tab-label').remove();
+			
+		});
+		
+		jQuery(this).find('.ig-shortcode-tab-buttons').find('a:first').addClass('active');
+		jQuery(this).find('.ig-shortcode-tabpane').hide();
+		jQuery(this).find('.ig-shortcode-tabpane:first').show();
+		
+	});
+	
+	var tab_to_show = 0;
+	jQuery('.ig-shortcode-tab-buttons a').live("click", function() {
+		tab_to_show = jQuery(this).parent('.ig-shortcode-tab-buttons').find('a').index(jQuery(this));
+		jQuery(this).parent('.ig-shortcode-tab-buttons').parent('.ig-shortcode-tabs').find('.ig-shortcode-tabpane').hide();
+		jQuery(this).parent('.ig-shortcode-tab-buttons').parent('.ig-shortcode-tabs').find('.ig-shortcode-tabpane').eq(tab_to_show).show();
+		jQuery(this).parent('.ig-shortcode-tab-buttons').find('a').removeClass('active');
+		jQuery(this).parent('.ig-shortcode-tab-buttons').find('a').eq(tab_to_show).addClass('active');
+		return false;
+	});
+});
