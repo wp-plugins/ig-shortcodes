98f6ce4487e22294c22fce28ac6780dc3c4f76dd
iografica <iografica@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 09:51:01 +0000

    new version js
    git-svn-id: https://plugins.svn.wordpress.org/ig-shortcodes/tags/1.6@1012017 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/mce-button.js
@@ -0,0 +1,112 @@
+(function () {
+	tinymce.PluginManager.add('ig_mce_button', function( editor, url ) {
+		editor.addButton( 'ig_mce_button',{
+			text: 'IG Shortcodes',
+			icon: false,
+			type: 'menubutton',
+			menu: [
+                // ACCORDION
+				{
+					text: 'Accordion',
+					onclick: function() {
+                    editor.insertContent('[ig_accordion_content]\
+                                          [ig_toggle heading="Title 1" onload="closed"]Content[/ig_toggle]\
+					                      [ig_toggle heading="Title 2" onload="closed"]Content[/ig_toggle]\
+					                      [/ig_accordion_content]');	
+				    },
+                },
+				// BUTTONS
+				{
+					text: 'Button',
+					onclick: function() {
+					editor.insertContent('[ig_button size="medium" color="gray" style="normal" link="http://themes.iografica.it" target="_blank"]more info[/ig_button]');
+					},
+				},
+				// COLUMNS
+                {
+                    text: 'Columns',
+                    menu: [
+                    {
+                        text: '1/2-1/2',
+                        onclick: function() {
+					    editor.insertContent('[ig_columns]\
+                                              [ig_column position="half first"]Content[/ig_column]\
+                                              [ig_column position="half last"]Content[/ig_column]\
+                                              [/ig_columns]');						
+					},
+                    },
+                    {
+                        text: '1/3-1/3-1/3',
+                        onclick: function() {
+                        editor.insertContent('[ig_columns]\
+                                              [ig_column position="one-third first"]Content[/ig_column]\
+                                              [ig_column position="one-third"]Content[/ig_column]\
+                                              [ig_column position="one-third last"]Content[/ig_column]\
+                                              [/ig_columns]');						
+					},
+                    },
+                    {
+                        text: '1/4-1/4-1/4-1/4',
+                        onclick: function() {
+                        editor.insertContent('[ig_columns]\
+                                             [ig_column position="one-fourth first"]Content[/ig_column]\
+                                             [ig_column position="one-fourth"]Content[/ig_column]\
+                                             [ig_column position="one-fourth"]Content[/ig_column]\
+                                             [ig_column position="one-fourth last"]Content[/ig_column]\
+                                             [/ig_columns]');						
+					},
+                    }
+                           ]
+                },
+				// CLLEARFIX
+				{
+					text: 'Clearfix',
+					onclick: function() {
+					editor.insertContent('[ig_clearfix]');						
+					},	
+				},
+				// CLLEARFIX
+				{
+					text: 'Divider',
+					onclick: function() {
+					editor.insertContent('[ig_divider style="normal"]');						
+					},	
+				},
+                // ICONS
+				{
+					text: 'Icon',
+					onclick: function() {
+					editor.insertContent('[ig_icon icon="genericon-rating-empty" size="normal"]');						
+					},		
+				},
+				// NOTICEBOX
+				{
+					text: 'Notice Box',
+					onclick: function() {
+					editor.insertContent('[ig_notice color="gray"]Content[/ig_notice]');						
+					},		
+				},
+				// TAB
+				{
+					text: 'Tabs',
+					onclick: function() {
+					editor.insertContent('[ig_tabs][ig_tab label="Title 1"]Content 1[/ig_tab][ig_tab label="Title 2"]Content 2[/ig_tab][/ig_tabs]');						
+					},	
+				},
+				// TOGGLE
+				{
+					text: 'Toggle',
+					onclick: function() {
+					editor.insertContent('[ig_toggle heading="Title" onload="closed"]Content[/ig_toggle]');						
+					},	
+				},
+                {
+                    text: '...more shortcodes',
+                    onclick: function() {
+                    window.open('http://themes.iografica.it/downloads/ig-shortcodes-premium/','_blank');
+                    },
+                },	
+			]
+		});
+	});
+})();
