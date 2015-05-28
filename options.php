dd676db07a8ab8213cb8e909abbdfc381757c978
iografica <iografica@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-28 12:03:28 +0000

    2.4 v
    git-svn-id: https://plugins.svn.wordpress.org/ig-shortcodes/tags/2.4@1169338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,59 @@
+<?php
+add_action( 'admin_menu', 'ig_shortcodes_register_menu_page' );
+
+function ig_shortcodes_register_menu_page(){
+    add_menu_page( 'IG Shortcodes', 'IG Shortcodes', 'manage_options', 'ig-shortcodes', 'ig_shortcodes_menu_page', 'dashicons-editor-code' );
+}
+
+function ig_shortcodes_menu_page() { ?>
+  <div class="wrap">
+  <h2><?php echo __('IG Shortcodes','ig-shortcodes'); ?></h2>
+<div class="menu">
+  <a href="http://www.iograficathemes.com/downloads/ig-shortcodes/"><?php echo __('Product page','ig-shortcodes'); ?></a> | <a href="http://www.iograficathemes.com/document/shortcodes-attributes/"><?php echo __('Documentation','ig-shortcodes'); ?></a> | <a href="https://wordpress.org/support/plugin/ig-shortcodes/"><?php echo __('Support','ig-shortcodes'); ?></a> | <a href="https://wordpress.org/plugins/ig-shortcodes/"><?php echo __('Changelog','ig-shortcodes'); ?></a>
+</div>
+      <div class="widget-liquid-left">
+            <h3><?php echo __('Shortcodes features','ig-shortcodes'); ?></h3>
+            <ul>
+                <li><?php echo __('Amazing shortcodes','ig-shortcodes'); ?></li>
+                <li><?php echo __('Power of CSS3 transitions','ig-shortcodes'); ?></li>
+                <li><?php echo __('Powerful shortcodes customization','ig-shortcodes'); ?></li>
+                <li><?php echo __('SEO Optimized','ig-shortcodes'); ?></li>
+                <li><?php echo __('Documented','ig-shortcodes'); ?></li>
+                <li><?php echo __('More features will to come...','ig-shortcodes'); ?></li>
+            </ul>
+          <h3><?php echo __('Iografica Themes', 'ig-shortcodes'); ?></h3>
+                    <a title="Facebook" href="https://www.facebook.com/iograficathemes" target="_blank">
+                    <span class="facebook"><?php echo __('Facebook', 'ig-shortcodes'); ?></span>
+                    </a>
+                    <?php echo __(' | ', 'ig-shortcodes'); ?>
+                    <a title="Twitter" href="https://twitter.com/iograficathemes" target="_blank">
+                    <span class="twitter"><?php echo __('Twitter','ig-shortcodes'); ?></span>
+                    </a>
+                    <?php echo __(' | ', 'ig-shortcodes'); ?>
+                    <a title="Google Plus" href="https://plus.google.com/+Iograficathemes/" target="_blank">
+                    <span class="google"><?php echo __('Google Plus','ig-shortcodes'); ?></span>
+                    </a>
+                    <?php echo __(' | ', 'ig-shortcodes'); ?>
+                    <a title="Iografica Themes" href="http://www.iograficathemes.com/" target="_blank">
+                    <span class="website"><?php echo __('Website','ig-shortcodes'); ?></span>
+                    </a>
+        </div>
+
+<div class="widget-liquid-right">
+    <div style="padding:0 15px 15px 15px; margin-top:15px; border:1px solid #dedede; background:#fff;">
+    <h3><?php echo __('IG Shotcodes Premium','ig-shortcodes'); ?></h3>
+     <p><?php echo __('Upgrade to premium version to have access to all shortcodes and priority support.','ig-shortcodes'); ?></p>
+    <h3><?php echo __('Shortcodes included','ig-shortcodes'); ?></h3>
+<ul>
+    <li><?php echo __('Author box','ig-shortcodes'); ?></li>
+    <li><?php echo __('Login box and protect content','ig-shortcodes'); ?></li>
+    <li><?php echo __('Post image gallery','ig-shortcodes'); ?></li>
+    <li><?php echo __('Pricing tables and testimonials','ig-shortcodes'); ?></li>
+    <li><?php echo __('Display WordPress widgets in content','ig-shortcodes'); ?></li>
+    <li><?php echo __('Related posts','ig-shortcodes'); ?></li>
+</ul>
+    <a class="button-primary" href="http://www.iograficathemes.com/downloads/ig-shortcodes-premium/"><?php echo __('Order Now','ig-shortcodes'); ?></a>
+     </div>
+    </div>
+</div>
+<?php } ?>
---
+++ readme.txt
@@ -0,0 +1,113 @@
+=== IG Shortcodes ===
+Contributors: iografica
+Tags: shortcode, shortcodes, responsive, tab, tabs, button, buttons, notice box, toggle, column, columns, tab, tabs, plugin, google maps
+Requires at least: 3.5
+Tested up to: 4.2
+Stable tag: trunk
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A complete set of WordPress shortcodes to add beautiful and useful elements to your theme.
+
+== Description ==
+IG Shortcodes is a complete set of WordPress shortcodes to add beautiful and useful elements to your theme.
+With IG Shortcodes you can turn your free theme to premium in just a few clicks.
+
+= Shortcodes inluded: =
+* Accordion
+* Buttons
+* Notice box
+* Columns
+* Clearfix
+* Divider
+* Tabs
+* Toggle
+* Heading
+* Icons
+* Google maps
+
+= IG Shortcodes Premium =
+For more shortcodes you can download the [premium version](http://iograficathemes.com/downloads/ig-shortcodes-premium/)
+
+= Extra shortcodes in premium version: =
+* Login module
+* Logged in user content
+* Author box
+* Pricing table
+* Posts image gallery
+* Wordpress default widgets display
+* Related posts
+
+= Available languages =
+* Italian
+* Serbo-Croatian
+* Spanish
+* Have a translation? Contact us and submit your translation!
+
+== Installation ==
+1. Download the plugin.
+2. Unzip the folder into the `/wp-content/plugins/` directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+For plugin documentation visit: http://iograficathemes.com/document/shortcodes-attributes/
+
+== Screenshots ==
+1. screenshot-1
+
+== Changelog ==
+= 1.0 =
+* Initial release
+
+= 1.1 =
+* Fix css for responsive tabs.
+
+= 1.2 =
+* Rebuilt the entire plugin. Please read the changelog before update the plugin. More shortcodes will be added on the next updates.
+
+= 1.3 =
+* Add more button styles (clean and puffy style).
+* Toggles css fix.
+* Add responsive css.
+
+= 1.4 =
+* Fix toggle “open” attribute.
+* Fix editor bug.
+
+= 1.5 =
+* Minor css fix
+* Add accordion shortcode.
+* Add heading shortcode.
+* Add icon shortcode.
+
+= 1.6 =
+* fix textdomain function error.
+* Minor css icon fix.
+* Add divider shortcode.
+
+= 1.7 =
+* Minor css fix.
+* Toggle bug fix.
+
+= 1.8 =
+* Fix author link.
+
+= 1.9 =
+* Minor fix.
+
+= 2.0 =
+* Minor options fix.
+* Upadated language .po file.
+* Added Serbo-Croatian language thanks to Borisa Djuraskovic of Web Hosting Hub community team.
+
+= 2.1 =
+* Added Google Maps shortcode.
+
+= 2.2 =
+* Minor fix.
+
+= 2.3 =
+* Added Spanish language thanks to Borisa Djuraskovic of FirstSiteGuide.
+
+= 2.4 =
+* Added plugin class.
